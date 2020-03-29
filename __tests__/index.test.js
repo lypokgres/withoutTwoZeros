37cea29ad710677140d65cb153d590f22799ec58
Serg Kopylov <lypokgres@gmail.com>
2020-03-29 12:53:47 +0700

    add test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -5,4 +5,5 @@ test('withoutTwoZeros', () => {
     expect(withoutTwoZeros(3, 1)).toBe(0);
     expect(withoutTwoZeros(4, 6)).toBe(35);
     expect(withoutTwoZeros(6, 8)).toBe(84);
+    expect(withoutTwoZeros(5, 6)).toBe(21);
 });
